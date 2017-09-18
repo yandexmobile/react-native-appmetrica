6e9449cb3d502eb37607c52dfe1191ca68fc6094
Aleksei Androsov <doochik@ya.ru>
2017-09-18 10:04:19 +0300

    Initial JS implementation

---
+++ index.js
@@ -0,0 +1,22 @@
+import { NativeModules } from 'react-native';
+const { AppMetrica } = NativeModules;
+
+export default {
+
+    /**
+     * Starts the statistics collection process.
+     * @param {string} apiKey
+     */
+    activateWithApiKey(apiKey) {
+        AppMetrica.activateWithApiKey(apiKey);
+    },
+
+    /**
+     * Sends a custom event message and additional parameters (optional).
+     * @param {string} message
+     * @param {object} [params=null]
+     */
+    reportEvent(message, params = null) {
+        AppMetrica.reportEvent(message, params);
+    },
+};
